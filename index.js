3839380d34111dd10c89a1b6fb59c4c0728fe4dc
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-04-22 22:45:49 -0600

    filiter

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching (drivers, searchName) {
+    return drivers.filter(driver => driver.toLowerCase().includes(searchName.toLowerCase()));
+  }
+  function fuzzyMatch(drivers, searchName) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(searchName.toLowerCase()));
+  }
+  
+    function matchName(drivers, searchName) {
+        return drivers.filter(driver => driver.name.toLowerCase() === searchName.toLowerCase());
+      }
+      
+
+    
